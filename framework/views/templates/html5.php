54bd4fd6c48da0e4601c8443eb1f42be508640d4
Gilles FELIX <felix@novius.com>
2012-03-23 14:45:34 +0100

    Remove in template IE's conditions in tag <html>

--- framework/views/templates/html5.php
+++ framework/views/templates/html5.php
@@ -10,11 +10,7 @@
 
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7 ]> <html class="ie ie6 lte9 lte8 lte7"> <![endif]-->
-<!--[if IE 7 ]>    <html class="ie ie7 lte9 lte8 lte7"> <![endif]-->
-<!--[if IE 8 ]>    <html class="ie ie8 lte9 lte8"> <![endif]-->
-<!--[if IE 9 ]>    <html class="ie ie9 lte9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html class=""> <!--<![endif]-->
+<html>
 <head>
 <?php
 	if (isset($base)) {
