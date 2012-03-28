1500374da5b7c18eff9b9fac05a949f06f625c95
Gilles FELIX <felix@novius.com>
2012-03-28 14:35:11 +0200

    Hooks css and js OK

---
+++ hooks/minify.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+files=`git diff --cached --name-status`
+
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../
+
+re="static/admin/novius-os/css/"
+if [[ $files =~ $re ]]
+then
+  ./hooks/minify-nos-css.sh
+  git add static/admin/novius-os/css/*
+fi
+
+re="static/admin/novius-os/js/"
+if [[ $files =~ $re ]]
+then
+  ./hooks/minify-nos-js.sh
+  git add static/admin/novius-os/js/*
+fi
