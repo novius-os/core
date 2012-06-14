c7e987af198ab9bd9705c1a9320d75c6264abe83
Gilles FELIX <felix@novius.com>
2012-06-14 15:51:41 +0200

    Add wijradio to vendor bundle

--- hooks/minify-vendor-js.sh
+++ hooks/minify-vendor-js.sh
@@ -10,7 +10,7 @@ export JAVA_CMD
 JS_COMBINED_FILE="vendor.min.js"
 PLUGIN_FILES=( log jquery/bgiframe/jquery.bgiframe-2.1.3-pre jquery/cookie/jquery.cookie jquery/globalize/globalize.min jquery/mousewheel/jquery.mousewheel.min jquery/jquery-form/jquery.form.min )
 UI_FILES=( ui.core ui.widget ui.position ui.mouse ui.draggable ui.droppable ui.resizable ui.sortable ui.dialog ui.button ui.datepicker effects.core effects.blind effects.slide effects.scale effects.transfer effects.fade )
-WIJMO_FILES=( wijmo.wijutil plugin.wijtextselection wijmo.wijinputcore wijmo.wijinputdate wijmo.wijinputmask wijmo.wijinputnumber wijmo.wijlist wijmo.wijpopup wijmo.wijcalendar wijmo.wijtabs wijmo.wijdatasource wijmo.wijsplitter wijmo.wijsuperpanel wijmo.wijdropdown wijmo.wijmenu wijmo.wijcheckbox wijmo.wijexpander wijmo.wijaccordion wijmo.wijdialog wijmo.wijpager wijmo.wijtextbox wijmo.wijgrid wijmo.wijlightbox wijmo.wijtooltip )
+WIJMO_FILES=( wijmo.wijutil plugin.wijtextselection wijmo.wijinputcore wijmo.wijinputdate wijmo.wijinputmask wijmo.wijinputnumber wijmo.wijlist wijmo.wijpopup wijmo.wijcalendar wijmo.wijtabs wijmo.wijdatasource wijmo.wijsplitter wijmo.wijsuperpanel wijmo.wijdropdown wijmo.wijmenu wijmo.wijcheckbox wijmo.wijradio wijmo.wijexpander wijmo.wijaccordion wijmo.wijdialog wijmo.wijpager wijmo.wijtextbox wijmo.wijgrid wijmo.wijlightbox wijmo.wijtooltip )
 
 
 #clear the files
