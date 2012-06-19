60a880d86b86e2cd71e398005c308442f3a47efb
esperat <esperat@novius.fr>
2012-06-19 17:43:58 +0200

    Added a blank_slate for the front office

---
+++ framework/views/errors/blank_slate_front.view.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>Novius OS welcomes you</title>
+<meta name="robots" content="noindex,nofollow">
+<link rel="shortcut icon" href="static/novius-os/admin/novius-os/img/noviusos.ico">
+
+<style type="text/css">
+html {
+    height : 100%;
+}
+body {
+  background: url("static/novius-os/admin/novius-os/img/wallpapers/circles.jpg");
+  background-size: cover;
+  font-family: franklin gothic medium,arial,verdana,helvetica,sans-serif;
+}
+#blank_slate {
+  background: rgba(255, 255, 255, 0.5);
+  border: 1px outset rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  padding: 20px 40px;
+  position: absolute;
+  top: 50px;
+  left: 50px;
+  right: 50px;
+  bottom: 50px;
+  overflow: auto;
+}
+#blank_slate h1, #blank_slate img {
+    vertical-align: middle;
+    padding: 0 2em 0 1em;
+}
+</style>
+
+</head>
+
+
+<body>
+    <div id="blank_slate">
+        <h1><img src="static/novius-os/admin/novius-os/img/logo.png">Novius OS is up and running. Now what?</h1>
+
+        <h2>I want a <strong>website</strong></h2>
+        <p>Novius OS is a CMS: you can create pages and manage your content. Once set up, your website will show here.</p>
+        <p><a href="https://github.com/novius-os/novius-os/wiki/Front-office" target="_blank">Check out our guide</a> to get started.</p>
+
+        <h2>I want an <strong>application</strong></h2>
+        <p>Novius OS is a web OS and framework: you can create applications that fits your business logic.         </p>
+        <p>A guide will be available in the near future to explain how to create your first application.         </p>
+
+        <h2>I don't know</h2>
+        <p><a href="admin">Go to the administration panel</a>, it's a good place to start.</p>
+    </div>
+</body>
+</html>
