2883e290db4d101ea2c19f68976253378b28e36e
esperat <esperat@novius.fr>
2012-06-21 18:56:11 +0200

    Refactoring Widget_Date_Picker, start

--- framework/classes/widget/date/picker.php
+++ framework/classes/widget/date/picker.php
@@ -10,65 +10,109 @@
 
 namespace Nos;
 
-class Widget_Date_Picker extends \Fieldset_Field {
+class Widget_Date_Picker extends \Fieldset_Field
+{
+    protected static $DEFAULT_WIDGET_OPTIONS = array(
+        'datepicker' => array(
+            'showOn' => 'both',
+            'buttonImage' => 'static/novius-os/admin/novius-os/img/icons/date-picker.png',
+            'buttonImageOnly' => true,
+            'autoSize' => true,
+            'dateFormat' => 'yy-mm-dd',
+            'showButtonPanel' => true,
+            'changeMonth' => true,
+            'changeYear' => true,
+            'showOtherMonths' => true,
+            'selectOtherMonths' => true,
+            'gotoCurrent' => true,
+            'firstDay' => 1,
+            'showAnim' => 'slideDown',
+        ),
+        'wrapper' => '', //'<div class="datepicker-wrapper"></div>',
+    );
 
-	protected $options = array(
-		'showOn'            => 'both',
-		'buttonImage'       => 'static/novius-os/admin/novius-os/img/icons/date-picker.png',
-		'buttonImageOnly'   => true,
-		'autoSize'          => true,
-		'dateFormat'        => 'yy-mm-dd',
-		'showButtonPanel'   => true,
-		'changeMonth'       => true,
-		'changeYear'        => true,
-		'showOtherMonths'   => true,
-		'selectOtherMonths' => true,
-		'gotoCurrent'       => true,
-		'firstDay'          => 1,
-		'showAnim'          => 'slideDown',
-        'wrapper'           => '', //'<div class="datepicker-wrapper"></div>',
-	);
+    /**
+     * Standalone build of the media widget.
+     *
+     * @param   array   $widget  Widget definition (attributes + widget_options)
+     * @return  string  The <input> tag + JavaScript to initialise it
+     */
+    public static function widget($widget = array())
+    {
+        list($attributes, $widget_options) = static::parse_options($widget);
+        $attributes['data-datepicker-options'] = htmlspecialchars(\Format::forge()->to_json($widget_options['datepicker']));
+        return '<input '.array_to_attr($attributes).' />'.static::js_init($attributes['id'], $widget_options);
+    }
 
-    public function __construct($name, $label = '', array $attributes = array(), array $rules = array(), \Fuel\Core\Fieldset $fieldset) {
+    protected $options = array();
 
-        $attributes['type']  = 'text';
-		$attributes['class'] = (isset($attributes['class']) ? $attributes['class'] : '').' datepicker';
-
-		if (empty($attributes['id'])) {
-			$attributes['id'] = uniqid('date_');
-		}
-		if (!empty($attributes['widget_options'])) {
-			$this->options = \Arr::merge($this->options, $attributes['widget_options']);
-		}
-		unset($attributes['widget_options']);
-
-        if (empty($attributes['size'])) {
-            $attributes['size'] = 9;
-        }
-
+    public function __construct($name, $label = '', array $widget = array(), array $rules = array(), \Fuel\Core\Fieldset $fieldset = null)
+    {
+        list($attributes, $this->options) = static::parse_options($widget);
         parent::__construct($name, $label, $attributes, $rules, $fieldset);
     }
 
     /**
      * How to display the field
-     * @return string
+     * @return type
      */
-    public function build() {
+    public function build()
+    {
         parent::build();
+        $this->fieldset()->append(static::js_init($this->get_attribute('id'), $this->options));
+        $datepicker_options = $this->options['datepicker'];
+        $attributes['data-datepicker-options'] = htmlspecialchars(\Format::forge()->to_json($datepicker_options));
 
-	    $this->fieldset()->append($this->js_init());
-
-		$this->set_attribute('data-datepicker-options', htmlspecialchars(\Format::forge()->to_json($this->options)));
+        $this->set_attribute('data-datepicker-options', htmlspecialchars(\Format::forge()->to_json($this->options)));
         return (string) parent::build();
     }
 
-	public function js_init() {
-        $wrapper = \Arr::get($this->options, 'wrapper', '');
-        unset($this->options['wrapper']);
-        return \View::forge('widget/date_picker', array(
-            'wrapper' => $wrapper,
-            'id' => $this->get_attribute('id'),
-        ), false);
-	}
+    /**
+     * Parse the widget array to get attributes and the widget options
+     * @param   array  $widget
+     * @return  array  0: attributes, 1: widget options
+     */
+    protected static function parse_options($widget = array())
+    {
+        $widget['type'] = 'text';
+        $widget['class'] = (isset($widget['class']) ? $widget['class'] : '').' datepicker';
 
+        if (empty($widget['id']))
+        {
+            $widget['id'] = uniqid('datepicker_');
+        }
+
+        if (empty($attributes['size']))
+        {
+            $attributes['size'] = 9;
+        }
+
+        // Default options of the widget
+        $widget_options = static::$DEFAULT_WIDGET_OPTIONS;
+
+        if (!empty($widget['widget_options']))
+        {
+            $widget_options = \Arr::merge($widget_options, $widget['widget_options']);
+        }
+        unset($widget['widget_options']);
+
+        return array($widget, $widget_options);
+    }
+
+
+    /**
+     * Generates the JavaScript to initialise the widget
+     *
+     * @param   string  HTML ID attribute of the <input> tag
+     * @return  string  JavaScript to execute to initialise the widget
+     */
+    protected static function js_init($id, $widget_options = array())
+    {
+        return \View::forge('widget/date_picker', array(
+            'id' => $id,
+            'wrapper' => \Arr::get($widget_options, 'wrapper', ''),
+        ), false);
+    }
 }
+
+
