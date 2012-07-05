1596d79fda395ee683bce73fa4d96f5eef4100ec
Gilles FELIX <felix@novius.com>
2012-07-05 11:07:44 +0200

    Fixed merge dev

--- framework/config/metadata.config.php
+++ framework/config/metadata.config.php
@@ -12,19 +12,19 @@ return array(
     'launchers' => array(
         'nos_page' => array(
             'name' => 'Page',
-            'url' => 'admin/nos/page/list/index',
+            'url' => 'admin/nos/page/appdesk/index',
             'iconUrl' => 'static/novius-os/admin/novius-os/img/32/page.png',
             'icon64' => 'static/novius-os/admin/novius-os/img/64/page.png',
         ),
         'nos_media' => array(
             'name' => 'Media centre',
-            'url' => 'admin/nos/media/list/index',
+            'url' => 'admin/nos/media/appdesk/index',
             'iconUrl' => 'static/novius-os/admin/novius-os/img/32/media.png',
             'icon64' => 'static/novius-os/admin/novius-os/img/64/media.png',
         ),
         'nos_user' => array(
             'name' => 'Users',
-            'url' => 'admin/nos/user/list/index',
+            'url' => 'admin/nos/user/appdesk/index',
             'iconUrl' => 'static/novius-os/admin/novius-os/img/32/user.png',
             'icon64' => 'static/novius-os/admin/novius-os/img/64/user.png',
         ),
