7d9576a2b9ef8471e2810bbc9f10048cbc9d826c
esperat <esperat@novius.fr>
2012-08-30 18:13:45 +0200

    Application manager: positive texts and nice buttons for actions

--- framework/views/admin/tray/app_manager.view.php
+++ framework/views/admin/tray/app_manager.view.php
@@ -22,39 +22,39 @@
 	<div class="unit col c1"></div>
 	<div class="unit col c10" id="line_first" style="position:relative;;">
         <div class="line" style="overflow:visible;">
-            <h1 class="title"><?= Nos\I18n::get('Core configuration'); ?></h1>
+            <h1 class="title"><?= __('Novius OS framework configuration'); ?></h1>
             <p>
                 <?php
                 if ($nos->is_dirty()) {
-                    echo 'Some modifications are not live - <a href="#" data-app="'.htmlspecialchars(\Format::forge(array('name' => 'nos', 'action' => 'add'))->to_json()).'" onclick="return false;">click to repair</a>';
+                    echo __('It looks like you made some changes recently.').' <a href="#" data-app="'.htmlspecialchars(\Format::forge(array('name' => 'nos', 'action' => 'add'))->to_json()).'" onclick="return false;"><button data-icon="wrench">'.__('Update configuration').'</button></a>';
                 } else {
-                    echo 'No problem detected!';
+                    echo __('Up to date!');
                 }
                 ?>
             </p>
         </div>
         <p>&nbsp;</p>
         <div class="line" style="overflow:visible;">
-			<h1 class="title"><?= Nos\I18n::get('Local configuration'); ?></h1>
+			<h1 class="title"><?= __('Local configuration'); ?></h1>
             <p>
             <?php
             if ($local->is_dirty()) {
-                echo 'Some modifications are not live - <a href="#" data-app="'.htmlspecialchars(\Format::forge(array('name' => 'local', 'action' => 'add'))->to_json()).'" onclick="return false;">click to repair</a>';
+                echo __('It looks like you made some changes recently.').' <a href="#" data-app="'.htmlspecialchars(\Format::forge(array('name' => 'local', 'action' => 'add'))->to_json()).'" onclick="return false;"><button data-icon="wrench">'.__('Update configuration').'</button></a>';
             } else {
-                echo 'No problem detected!';
+                echo __('Up to date!');
             }
             ?>
             </p>
         </div>
         <p>&nbsp;</p>
 		<div class="line" style="overflow:visible;">
-			<h1 class="title"><?= Nos\I18n::get('Applications'); ?></h1>
+			<h1 class="title"><?= __('Applications'); ?></h1>
 
 			<div class="app_list">
 				<table>
 					<thead>
 						<tr>
-							<td><?= Nos\I18n::get('Installed and ready to use') ?></td>
+							<td><?= __('Installed and ready to use') ?></td>
 							<td><?= __('Actions') ?></td>
 						</tr>
 					</thead>
@@ -63,10 +63,15 @@
                             $metadata = $app->metadata;
                             ?>
 						<tr>
-							<td><?= e($app->name) ?></td>
+							<td>&nbsp;<?= isset($metadata['icon16']) ? '<img src="'.$metadata['icon16'].'" style="vertical-align:top;" alt="" title="" />' : '' ?> <?= e($app->name) ?></td>
 							<td>
-								<a href="#" data-app="<?= htmlspecialchars(\Format::forge(array('name' => $app->folder, 'action' => 'remove'))->to_json()) ?>" onclick="return false;">remove</a>
-								<?= $app->is_dirty() ? '- [<a href="#" data-app="'.htmlspecialchars(\Format::forge(array('name' => $app->folder, 'action' => 'add'))->to_json()).'" onclick="return false;">repair install</a>]' : '' ?>
+								<a href="#" data-app="<?= htmlspecialchars(\Format::forge(array('name' => $app->folder, 'action' => 'remove'))->to_json()) ?>" onclick="return false;"><button data-icon="arrowthick-1-s"><?= __('Uninstall') ?></button></a>
+								<?php
+                                if ($app->is_dirty()) {
+                                    ?>
+                                    <a href="#" data-app="<?= htmlspecialchars(\Format::forge(array('name' => $app->folder, 'action' => 'add'))->to_json()) ?>" onclick="return false;"><button data-icon="wrench"><?= __('Update') ?></button></a>
+                                    <?php
+                                } ?>
 							</td>
 						</tr>
 						<?php } ?>
@@ -74,7 +79,7 @@
 				</table>
 
 				<?php if (empty($installed)) { ?>
-				<em><?php echo Nos\I18n::get('No applications found') ?>.</em>
+				<em><?php echo __('No applications found') ?>.</em>
 				<?php } ?>
 			</div>
 
@@ -84,7 +89,7 @@
 				<table>
 					<thead>
 						<tr>
-							<td><?= Nos\I18n::get('Available for installation') ?></td>
+							<td><?= __('Available for installation') ?></td>
 							<td><?= __('Actions') ?></td>
 						</tr>
 					</thead>
@@ -94,27 +99,26 @@
                     ?>
 						<tr>
 							<td><?= e($app->name) ?> </td>
-							<td><a href="#" data-app="<?= htmlspecialchars(\Format::forge(array('name' => $app->folder, 'action' => 'add'))->to_json()) ?>" onclick="return false;">add</a></td>
+							<td><a href="#" data-app="<?= htmlspecialchars(\Format::forge(array('name' => $app->folder, 'action' => 'add'))->to_json()) ?>" onclick="return false;"><button data-icon="arrowthick-1-n"><?= __('Install') ?></button></a></td>
 						</tr>
 						<?php } ?>
 					</tbody>
 				</table>
 
 				<?php if (empty($others)) { ?>
-				<em><?= Nos\I18n::get('No applications found') ?></em>
+				<em><?= __('No applications found') ?></em>
 				<?php } ?>
 
 			</div>
 
 			<?php if ($allow_upload) { ?>
+                <p>&nbsp;</p>
+                <h1 class="title"><?= __('Install from a .zip file') ?></h1>
 
-			<p>&nbsp;</p>
-			<h1 class="title"><?= Nos\I18n::get('Install from a .zip file') ?></h1>
-
-			<form method="post" action="/admin/nos/tray/appmanager/upload" enctype="multipart/form-data">
-				<input type="file" name="zip" />
-				<input type="submit" value="Upload the application" />
-			</form>
+                <form method="post" action="/admin/nos/tray/appmanager/upload" enctype="multipart/form-data">
+                    <input type="file" name="zip" />
+                    <input type="submit" value="<?= __('Upload the application') ?>" />
+                </form>
 			<?php } ?>
 		</div>
 	</div>
