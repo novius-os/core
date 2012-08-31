80304d58e14215148ab3d18ce61c9e73854710d8
Gilles FELIX <felix@novius.com>
2012-08-31 15:25:49 +0200

    Code styling

--- framework/classes/fuel/fieldset_field.php
+++ framework/classes/fuel/fieldset_field.php
@@ -12,11 +12,15 @@ class Fieldset_Field extends \Fuel\Core\Fieldset_Field
 {
     public function populate($input, $repopulate = false)
     {
-        if (is_array($input)) { // or $input instanceof \ArrayAccess
-            if (isset($input[$this->name])) {
+        if (is_array($input))
+        { // or $input instanceof \ArrayAccess
+            if (isset($input[$this->name]))
+            {
                 $this->set_value($input[$this->name], true);
             }
-        } elseif (is_object($input)) // and property_exists($input, $f->name) {
+        }
+        elseif (is_object($input)) // and property_exists($input, $f->name)
+        {
             $this->set_value($input->{$this->name}, true);
         }
     }
@@ -24,10 +28,12 @@ class Fieldset_Field extends \Fuel\Core\Fieldset_Field
     public function repopulate(array $input)
     {
         // Don't repopulate the CSRF field
-        if ($this->name === \Config::get('security.csrf_token_key', 'fuel_csrf_token')) {
+        if ($this->name === \Config::get('security.csrf_token_key', 'fuel_csrf_token'))
+        {
             return;
         }
-        if (($value = \Arr::get($input, $this->name, null)) !== null) {
+        if (($value = \Arr::get($input, $this->name, null)) !== null)
+        {
            $this->set_value($value, true);
         }
     }
@@ -35,7 +41,8 @@ class Fieldset_Field extends \Fuel\Core\Fieldset_Field
     public function generate_auto_id()
     {
         $form = $this->fieldset()->form();
-        if ($form->get_config('auto_id', false) === true and $this->get_attribute('id') == '') {
+        if ($form->get_config('auto_id', false) === true and $this->get_attribute('id') == '')
+        {
             $auto_id = $form->get_config('auto_id_prefix', '').str_replace(array('[', ']', '->'), array('-', '', '_'), $this->name);
             $this->set_attribute('id', $auto_id);
         }
@@ -45,22 +52,27 @@ class Fieldset_Field extends \Fuel\Core\Fieldset_Field
     {
         $this->generate_auto_id();
         $required = $this->get_attribute('required', null);
-        if ($required) {
+        if ($required)
+        {
             $label = $this->label;
-            if (!is_array($label)) {
+            if (!is_array($label))
+            {
                 $label = array('label' => $label);
             }
-            if (empty($label['title'])) {
+            if (empty($label['title']))
+            {
                 $label['title'] = __('Mandatory');
             }
             $this->label = $label;
         }
-        if (isset($this->attributes['widget_options'])) {
+        if (isset($this->attributes['widget_options']))
+        {
             $widget_options = $this->attributes['widget_options'];
             unset($this->attributes['widget_options']);
         }
         $return = parent::build();
-        if (isset($widget_options)) {
+        if (isset($widget_options))
+        {
             $this->attributes['widget_options'] = $widget_options;
         }
 
