92d8bcc69fa0059ad6021a905c447b881d0cf51b
Sébastien Drouyer <drouyer@novius.com>
2012-09-18 10:07:12 +0200

    new tray

--- framework/views/admin/tray/right.view.php
+++ framework/views/admin/tray/right.view.php
@@ -1,50 +1,13 @@
-<?= render('admin/tray/right/standard_description') ?>
-
-<?php
-
-?>
-
-<a class="button" onclick="$(this).nosTabs('open', {url: 'admin/nos/tray/help'});">
-    <button data-icon="help">
-        <?= __('Help') ?>
-    </button>
+<a class="button" style="cursor: pointer;" title="<?= __('Help') ?>" onclick="$(this).nosTabs('open', {url: 'admin/nos/tray/help', app: true, iconSize: 32, labelDisplay: false, iconUrl: 'static/novius-os/admin/novius-os/img/32/help.png'});">
+    <img src="static/novius-os/admin/novius-os/img/32/help.png" />
 </a>
-
-<a class="button" onclick="$(this).nosTabs('open', {url: 'admin/nos/tray/appmanager'});">
-    <button data-icon="cart">
-        <?= __('Application manager') ?>
-    </button>
-</a>
-
-<a class="button" onclick="$(this).nosTabs('open', {url: 'admin/nos/tray/account'});">
-    <button data-icon="person">
-        <?= __('Account') ?>
-    </button>
-</a>
-
-<a class="button" onclick="$(this).nosTabs('open', {url: 'admin/nos/tray/account/disconnect'});">
-    <button data-icon="power">
-        <?= __('Disconnect') ?>
-    </button>
-</a>
-<?php
-
-?>
-
-<?php
-/*
-?>
 <ul id="menu" style="display: inline-block;">
-    <li><a><?= __('Applications') ?></a>
+    <li><a><?= \Session::user()->user_firstname ?></a>
         <ul>
-            <li><a onclick="$(this).nosTabs('open', {url: 'admin/nos/tray/appmanager'});"><?= __('Applications manager') ?></a></li>
-            <li><a>Application 1</a></li>
-            <li><a>Application 2</a></li>
+            <li><a onclick="$(this).nosTabs('open', {url: 'admin/nos/tray/account', app: true, iconSize: 32, labelDisplay: false, iconUrl: 'static/novius-os/admin/novius-os/img/32/myaccount.png'});"><?= __('My account') ?></a></li>
+            <li><a onclick="document.location = 'admin/nos/tray/account/disconnect';"><?= __('Log out') ?></a></li>
         </ul>
     </li>
-    <li><a onclick="$(this).nosTabs('open', {url: 'admin/nos/tray/account'});"><?= __('Account') ?></a></li>
-    <li><a onclick="$(this).nosTabs('open', {url: 'admin/nos/tray/help'});"><?= __('Help') ?></a></li>
-    <li><a onclick="$(this).nosTabs('open', {url: 'admin/nos/tray/account/disconnect'});"><?= __('Disconnect') ?></a></li>
 </ul>
 <script type="text/javascript">
     require(
@@ -55,6 +18,3 @@
             });
         });
 </script>
-<?php
-*/
-?>
