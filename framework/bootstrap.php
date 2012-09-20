d134c899ac0e2da495eb0c810a2f77919c7e2a2d
esperat <esperat@novius.fr>
2012-09-20 16:44:10 +0200

    Extending Fuel\Core\Validation_Error

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -65,6 +65,7 @@ if (!MBSTRING) {
     'Response'          => FUEL_EXTEND_PATH.'response.php',
     'Session'           => FUEL_EXTEND_PATH.'session.php',
     'Str'               => FUEL_EXTEND_PATH.'str.php',
+    'Validation_Error'  => FUEL_EXTEND_PATH.'validation_error.php',
     'View'              => FUEL_EXTEND_PATH.'view.php',
     'Nos\Oil\Console'   => FUEL_EXTEND_PATH.'oil'.DIRECTORY_SEPARATOR.'console.php',
     'Nos\Orm\Model'     => FUEL_EXTEND_PATH.'orm'.DIRECTORY_SEPARATOR.'model.php',
