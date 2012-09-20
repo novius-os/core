c7fe66271937f094f4390ed71da7ba7914335c0d
Gilles FELIX <felix@novius.com>
2012-09-20 11:48:14 +0200

    Fixed : data catchers absolute_url and media_url check page and media exist

--- framework/views/admin/data_catcher/applications.view.php
+++ framework/views/admin/data_catcher/applications.view.php
@@ -8,67 +8,68 @@
  * @link http://www.novius-os.org
  */
 
-    if (isset($nuggets[\Nos\DataCatcher::TYPE_IMAGE])) {
-        $nuggets['media_url'] = Uri::base(false).\Nos\Model_Media::find($nuggets[\Nos\DataCatcher::TYPE_IMAGE])->get_public_path();
+if (isset($nuggets[\Nos\DataCatcher::TYPE_IMAGE])) {
+    $image = \Nos\Model_Media::find($nuggets[\Nos\DataCatcher::TYPE_IMAGE]);
+    if (empty($image)) {
+        unset($nuggets[\Nos\DataCatcher::TYPE_IMAGE]);
+    } else {
+        $nuggets['media_url'] = Uri::base(false).$image->get_public_path();
     }
-    if (isset($nuggets[\Nos\DataCatcher::TYPE_URL])) {
-        list($page_id, $path) = preg_split("/\:\:/", $nuggets[\Nos\DataCatcher::TYPE_URL]);
-        if (!empty($path)) {
-            $page_path = preg_replace('`'.preg_quote('.html').'$`iUu', '', \Nos\Model_Page::find($page_id)->get_href(array('absolute' => true)));
+}
+if (isset($nuggets[\Nos\DataCatcher::TYPE_URL])) {
+    list($page_id, $path) = preg_split("/\:\:/", $nuggets[\Nos\DataCatcher::TYPE_URL]);
+    if (!empty($path)) {
+        $page = \Nos\Model_Page::find($page_id);
+        if (empty($page)) {
+            unset($nuggets[\Nos\DataCatcher::TYPE_URL]);
+        } else {
+            $page_path = preg_replace('`'.preg_quote('.html').'$`iUu', '', $page->get_href(array('absolute' => true)));
             $nuggets['absolute_url'] = rtrim($page_path, '/').'/'.$path;
         }
     }
+}
 
-    $onDemande = false;
-    $auto = false;
-    $one = false;
-    foreach ($data_catchers as $catcher_name => $data_catcher)
-    {
-        if (!is_array($data_catcher['required_data']))
-        {
-            $data_catcher['required_data'] = array();
+$onDemande = false;
+$auto = false;
+$one = false;
+foreach ($data_catchers as $catcher_name => $data_catcher) {
+    if (!is_array($data_catcher['required_data'])) {
+        $data_catcher['required_data'] = array();
+    }
+    $compatible = true;
+    foreach ($data_catcher['required_data'] as $type_data) {
+        if (empty($nuggets[$type_data])) {
+            $compatible = false;
+            break;
         }
-        $compatible = true;
-        foreach ($data_catcher['required_data'] as $type_data) {
-            if (empty($nuggets[$type_data]))
-            {
-                $compatible = false;
-                break;
-            }
-        }
-        if (!$compatible)
-        {
-            continue;
-        }
-        $one = true;
+    }
+    if (!$compatible) {
+        continue;
+    }
+    $one = true;
 
-        if (isset($data_catcher['onDemand']) && $data_catcher['onDemand'] && !$onDemande)
-        {
-            $onDemande = true;
-            echo '<div>', htmlspecialchars(strtr(__('"{item}" can be shared with the following applications.'), array('{item}' => $item->title_item()))) ,'</div>';
-            echo '<h4>', htmlspecialchars(__('Click to share:')) ,'</h4>';
-            echo '<small>', htmlspecialchars(__('(Don\'t worry, you\'ll get a preview first)')) ,'</small>';
-        }
-        elseif ((!isset($data_catcher['onDemand']) || !$data_catcher['onDemand']) && !$auto)
-        {
-            echo '<div>', htmlspecialchars(strtr(__('"{item}" is automatically shared with the following applications.'), array('{item}' => $item->title_item()))) ,'</div>';
-            echo '<h4>', htmlspecialchars(__('No action required, click to customise:')) ,'</h4>';
-            $auto = true;
-        }
-
-        if ($auto)
-        {
-            $data_catcher['url'] .= '?'.http_build_query(array(
-                'model' => $model_name,
-                'id'    => $model_id,
-                'catcher' => $catcher_name,
-             ), '', '&');
-        }
-
-        echo '<button class="catcher" data-params="', htmlspecialchars(\Format::forge($data_catcher)->to_json()) ,'" data-nuggets="', htmlspecialchars(\Format::forge($nuggets)->to_json()) ,'">', htmlspecialchars($data_catcher['title']),'</button>';
+    if (isset($data_catcher['onDemand']) && $data_catcher['onDemand'] && !$onDemande) {
+        $onDemande = true;
+        echo '<div>', htmlspecialchars(strtr(__('"{item}" can be shared with the following applications.'), array('{item}' => $item->title_item()))) ,'</div>';
+        echo '<h4>', htmlspecialchars(__('Click to share:')) ,'</h4>';
+        echo '<small>', htmlspecialchars(__('(Don\'t worry, you\'ll get a preview first)')) ,'</small>';
+    } elseif ((!isset($data_catcher['onDemand']) || !$data_catcher['onDemand']) && !$auto) {
+        echo '<div>', htmlspecialchars(strtr(__('"{item}" is automatically shared with the following applications.'), array('{item}' => $item->title_item()))) ,'</div>';
+        echo '<h4>', htmlspecialchars(__('No action required, click to customise:')) ,'</h4>';
+        $auto = true;
     }
 
-    if (!$one)
-    {
-        echo '<div>', htmlspecialchars(strtr(__('How sad! "{item}" cannot be shared with any application yet. Ask your developer to set you up some nice sharing applications.'), array('{item}' => $item->title_item()))) ,'</div>';
+    if ($auto) {
+        $data_catcher['url'] .= '?'.http_build_query(array(
+            'model' => $model_name,
+            'id'    => $model_id,
+            'catcher' => $catcher_name,
+         ), '', '&');
     }
+
+    echo '<button class="catcher" data-params="', htmlspecialchars(\Format::forge($data_catcher)->to_json()) ,'" data-nuggets="', htmlspecialchars(\Format::forge($nuggets)->to_json()) ,'">', htmlspecialchars($data_catcher['title']),'</button>';
+}
+
+if (!$one) {
+    echo '<div>', htmlspecialchars(strtr(__('How sad! "{item}" cannot be shared with any application yet. Ask your developer to set you up some nice sharing applications.'), array('{item}' => $item->title_item()))) ,'</div>';
+}
