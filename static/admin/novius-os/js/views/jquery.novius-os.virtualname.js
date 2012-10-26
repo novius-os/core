96672b6d1d45931b62888a8f59548d108ed693aa
Gilles FELIX <felix@novius.com>
2012-10-26 15:11:50 +0200

    Move some inline js in views in a js files

---
+++ static/admin/novius-os/js/views/jquery.novius-os.virtualname.js
@@ -0,0 +1,57 @@
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+define('jquery-nos-virtualname',
+    ['jquery-nos'],
+    function($) {
+        "use strict";
+
+        $.fn.extend({
+            nosVirtualName : function() {
+                return this.each(function() {
+                    var replace_url = function(str) {
+                                if (!str) {
+                                    return str;
+                                }
+
+                                return str.replace(/ /g, '-')
+                                    .replace(/[\?|:|\\|\/|\#|\[|\]|@]/g, '-')
+                                    .replace(/-{2,}/g, '-')
+                                    .replace(/-$/g, '')
+                                    .replace(/^-/g, '')
+                                    .toLowerCase();
+                            },
+                        $virtual_name = $(this),
+                        id = $virtual_name.attr('id'),
+                        $use_title_checkbox = $('#' + id + '__use_title_checkbox'),
+                        $title = $virtual_name.closest('form').find('input.title');
+
+                    if (replace_url($title.val()) == $virtual_name.val() || $virtual_name.val() == '') {
+                        $use_title_checkbox.attr('checked', true).wijcheckbox("refresh");
+                    }
+
+                    $use_title_checkbox.change(function() {
+                        if ($(this).is(':checked')) {
+                            $virtual_name.attr('readonly', true).addClass('ui-state-disabled').removeClass('ui-state-default');
+                            $title.triggerHandler('change');
+                        } else {
+                            $virtual_name.removeAttr('readonly').addClass('ui-state-default').removeClass('ui-state-disabled');
+                        }
+                    }).triggerHandler('change');
+
+                    $title.bind('change keyup', function() {
+                        if ($use_title_checkbox.is(':checked')) {
+                            $virtual_name.val(replace_url($title.val()));
+                        }
+                    });
+                });
+            }
+        });
+
+        return $;
+    });
