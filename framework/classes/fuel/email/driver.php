08a830c2a2eb7323a4780ad46376e6b54bf0ee92
Gilles FELIX <felix@novius.com>
2012-11-28 12:20:08 +0100

    Add package email and special implementation in Novius OS

--- framework/classes/fuel/email/driver.php
+++ framework/classes/fuel/email/driver.php
@@ -8,62 +8,18 @@
  * @link http://www.novius-os.org
  */
 
-class Email extends \Email\Email
+\Package::load('email');
+
+abstract class Email_Driver extends \Email\Email_Driver
 {
-    /**
-     * Compare two dates without regard to hours.
-     *
-     * @param   int|Date    first date to compare.
-     * @param   int|Date    seconda date to compare.
-     * @return  int			-1 If the first date is earlier, 1 if it's after, 0 if both dates are equal
-     */
-    public static function compare($date1, $date2)
+    public function send($validate = null)
     {
-        $date1 = ( ! $date1 instanceof Date) ? static::forge($date1) : $date1;
-        $date2 = ( ! $date2 instanceof Date) ? static::forge($date2) : $date2;
+        \Event::trigger('email.before_send', $this);
 
-        $date1 = $date1->format('%Y-%m-%d');
-        $date2 = $date2->format('%Y-%m-%d');
-        if ($date1 < $date2) {
-            return -1;
-        } elseif ($date1 > $date2) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }
+        $return = parent::send($validate);
 
+        \Event::trigger('email.after_send', $this);
 
-    /**
-     * Modify the timestamp of the date, by incrementing or decrementing in a format accepted by strtotime().
-     *
-     * @param   string   a date/time string. Valid formats are explained in PHP Manual chapter's "Date and Time Formats".
-     * @return  Date
-     */
-    public function modify($modify)
-    {
-        // Temporarily change timezone when different from default
-        if (\Fuel::$timezone != $this->timezone) {
-            date_default_timezone_set($this->timezone);
-        }
-
-        // Modify the timestamp
-        $timestamp = strtotime($modify, $this->timestamp);
-
-        if ($timestamp === false) {
-            \Error::notice('Invalid input for modify given.');
-            return false;
-        } else {
-            $this->timestamp = $timestamp;
-        }
-
-        // Change timezone back to default if changed previously
-        if (\Fuel::$timezone != $this->timezone) {
-            date_default_timezone_set(\Fuel::$timezone);
-        }
-
-        return $this;
+        return $return;
     }
 }
-
-/* End of file date.php */
