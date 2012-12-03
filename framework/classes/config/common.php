47975be96333ec05b74ea6aa87ae367cee11d33e
Gilles FELIX <felix@novius.com>
2012-12-03 17:31:33 +0100

    Bugfix : {{id}} placeholder in action not replace in toolbar crud

--- framework/classes/config/common.php
+++ framework/classes/config/common.php
@@ -53,7 +53,7 @@ class Config_Common
                 'action' => array(
                     'action' => 'nosTabs',
                     'tab' => array(
-                        'url' => "insert_update/{{id}}",
+                        'url' => "insert_update/{{_id}}",
                         'label' => __('Edit'),
                     ),
                 ),
@@ -68,7 +68,7 @@ class Config_Common
                 'action' => array(
                     'action' => 'confirmationDialog',
                     'dialog' => array(
-                        'contentUrl' => 'delete/{{id}}',
+                        'contentUrl' => 'delete/{{_id}}',
                         'title' => __('Delete'),
                     ),
                 ),
@@ -113,7 +113,7 @@ class Config_Common
                 'action' => array(
                     'action' => 'share',
                     'data' => array(
-                        'model_id' => '{{id}}',
+                        'model_id' => '{{_id}}',
                         'model_name' => '',
                     ),
                 ),
