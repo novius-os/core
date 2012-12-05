0c37ebf0120e042334bca454f26958311c69237e
Gilles FELIX <felix@novius.com>
2012-12-05 15:27:45 +0100

    Add 'About Novius OS' popup, with version

---
+++ framework/config/version.config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+
+return array(
+
+    'fullname'    => '0.2.0.0',
+
+    'date'            => \Date::forge()->create_from_string('2012-12-05', 'mysql_date'),
+);
