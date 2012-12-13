1dddb1b48e042b0bfaaa50d43753e911c90140b3
esperat <esperat@novius.fr>
2012-12-13 12:32:10 +0100

    Missing translation for valid_email

--- framework/config/validation.config.php
+++ framework/config/validation.config.php
@@ -14,6 +14,7 @@ return array(
     'min_length' => __('This field has to contain at least :param:1 characters.'),
     'max_length' => __('This field may not contain more than :param:1 characters.'),
     'valid_date' => __('The entered date is invalid.'),
+    'valid_email' => __('The entered email is invalid.'),
     'check_old_password' => __('The old password is incorrect.'),
     'match_field' =>  __('This field doesn\'t match.'),
 );
