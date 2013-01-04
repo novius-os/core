a1bda3efc6131495afcbeb19de9adbeda448c08f
Gilles FELIX <felix@novius.com>
2013-01-04 09:57:47 +0100

    Apply patch http://www.tinymce.com/develop/bugtracker_view.php?id=5522 on tinymce, remove old patch which use extended_valid_elements

--- static/admin/novius-os/js/jquery.novius-os.wysiwyg.js
+++ static/admin/novius-os/js/jquery.novius-os.wysiwyg.js
@@ -31,14 +31,7 @@ define('jquery-nos-wysiwyg',
                         plugins    : 'spellchecker,xhtmlxtras,style,table,advlist,inlinepopups,media,searchreplace,paste,noneditable,visualchars,nonbreaking',
                         paste_text_use_dialog : true,
                         theme_nos_enhancers : enhancers,
-                        theme_nos_context: self.closest('.nos-dispatcher').data('nosContext'),
-                        // extended_valid_elements: http://www.tinymce.com/wiki.php/Configuration:extended_valid_elements
-                        // Fixes issue: http://www.tinymce.com/develop/bugtracker_view.php?id=5522
-                        // Rule sets inspired by: http://www.tinymce.com/wiki.php/Configuration:valid_elements
-                        extended_valid_elements: "div[id|data-enhancer|data-config|class|style|title|dir<ltr?rtl|lang|xml::lang|onclick|ondblclick|"
-                            + "onmousedown|onmouseup|onmouseover|onmousemove|onmouseout|onkeypress|"
-                            + "onkeydown|onkeyup],img[longdesc|data-media|data-media-id|usemap|"
-                            + "src|border|style|alt=|title|hspace|vspace|width|height|align]"
+                        theme_nos_context: self.closest('.nos-dispatcher').data('nosContext')
                     }, options || {});
 
                     $(self).tinymce(options);
