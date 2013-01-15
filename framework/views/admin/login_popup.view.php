fdff3358231de3b77516765accc9143a170c0bb3
esperat <esperat@novius.fr>
2013-01-15 19:21:39 +0100

    Fixed XSS on login screen

--- framework/views/admin/login_popup.view.php
+++ framework/views/admin/login_popup.view.php
@@ -20,15 +20,14 @@ require(
             $container.closest(':wijmo-wijdialog').wijdialog({
                 title : <?= \Format::forge()->to_json(__("You've been inactive for too long")) ?>
             });
-            var $email = $('#<?= $uniqid ?>_email');
-            $email.select();
+            $('#<?= $uniqid ?>_email').select();
         });
     });
 </script>
 <div id="<?= $uniqid ?>" class="login_popup blank_slate">
     <img src="static/novius-os/admin/novius-os/img/logo.png" style="width: 245px;" alt="" />
     <form method="POST" action="/admin/nos/login/popup">
-        <p><input type="email" name="email" id="<?= $uniqid ?>_email" value="<?= \Input::post('email', ''); ?>" placeholder="<?= __('Email') ?>" /></p>
+        <p><input type="email" name="email" id="<?= $uniqid ?>_email" value="<?= e(\Input::post('email', '')); ?>" placeholder="<?= __('Email') ?>" /></p>
         <p><input type="password" name="password" placeholder="<?= __('Password') ?>" /></p>
         <p>
             <input type="checkbox" id="remember_me" name="remember_me" value="1" />
