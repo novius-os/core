fc452bb6f78120882ef499ceab07430da0f01fa5
Gilles FELIX <felix@novius.com>
2013-02-04 12:19:40 +0100

    Code styling

--- framework/classes/fuel/config_file.php
+++ framework/classes/fuel/config_file.php
@@ -4,12 +4,9 @@ abstract class Config_File extends \Fuel\Core\Config_File
 {
     public function load($overwrite = false)
     {
-        try
-        {
+        try {
             $config = parent::load($overwrite);
-        }
-        catch (\ConfigException $e)
-        {
+        } catch (\ConfigException $e) {
             $config = array();
         }
         Event::trigger_function('config|'.$this->group(), array(&$config));
