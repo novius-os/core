a10d7667076165c1df6874d18aa3198b321530bf
Gilles FELIX <felix@novius.com>
2013-02-25 14:18:34 +0100

    Remove profiler log, add profiler event

--- framework/classes/fuel/profiler.php
+++ framework/classes/fuel/profiler.php
@@ -3,14 +3,7 @@ class Profiler extends \Fuel\Core\Profiler
 {
     public static function start($dbname, $sql)
     {
-        $backtrace = \Debug::local_backtrace(true);
-        $profiled = array();
-        for ($i = 0; $i < count($backtrace); $i++) {
-            $profiled[] = $backtrace[$i]['file'].'@'.$backtrace[$i]['line'];
-        }
-        /*\Log::info(print_r(array_keys($backtrace[0][0]), true));
-        exit();*/
-        \Log::info('SQL Request executed: "'.$sql."\"\nLocal backtrace:\n".implode("\n", $profiled)."\n\n");
+        \Event::trigger_function('profiler.sql', array(array('dbname' => $dbname, 'sql' => $sql)));
 
         return parent::start($dbname, $sql);
     }
