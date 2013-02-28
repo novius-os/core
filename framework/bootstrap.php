ece0cf53b1e6926aa6e44081455b1c4a1985b6be
Julian Esperat <esperat@novius.fr>
2013-02-28 14:46:36 +0100

    Bugfix: accented URLs were Unreachable

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -174,7 +174,7 @@ foreach (Config::get('namespaces', array()) as $ns => $path) {
 chdir(DOCROOT);
 
 // Remove leading /
-$_SERVER['NOS_URL'] = mb_substr($_SERVER['REQUEST_URI'], 1);
+$_SERVER['NOS_URL'] = mb_substr(urldecode($_SERVER['REQUEST_URI']), 1);
 if (defined('NOS_RELATIVE_DIR')) {
     $_SERVER['NOS_URL'] = mb_substr($_SERVER['NOS_URL'], mb_strlen(NOS_RELATIVE_DIR));
 }
