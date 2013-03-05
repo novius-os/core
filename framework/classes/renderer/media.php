399e0152d4d8df493d8d7d0a6001a1ad81eda2e1
Gilles FELIX <felix@novius.com>
2013-03-05 14:25:30 +0100

    Add \Nos\Renderer_Media in deprecated

---
+++ framework/classes/renderer/media.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+
+namespace Nos;
+
+
+/**
+ * @deprecated This class is deprecated.  Please use \Nos\Media\Renderer_Media instead.
+ */
+class Renderer_Media extends \Nos\Media\Renderer_Media
+{
+}
+
+logger(\Fuel::L_WARNING, 'This class is deprecated.  Please use \Nos\Media\Renderer_Media instead.');
+
