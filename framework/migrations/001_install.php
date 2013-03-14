a5893de8c91680e4c21659af0f539fad01230537
Sébastien Drouyer <drouyer@novius.com>
2013-03-14 11:50:05 +0100

    changed legacy install migration : fix on install native application

--- framework/migrations/001_install.php
+++ framework/migrations/001_install.php
@@ -12,6 +12,38 @@ class Install extends \Nos\Migration
             $sql_file_legacy = substr($this->path, 0, strlen($this->path) - 4).'_legacy.sql';
             static::execute_sql_file($sql_file_legacy);
             \Config::set('migrations.app.default', array());
+            \Config::set('migrations.version.package', array(
+                'nos' =>
+                array(
+                    0 => '001_install',
+                    1 => '002_version_0_2',
+                    2 => '003_move_migration_table',
+                ),
+            ));
+            \Config::set('migrations.version.module', array(
+                'noviusos_media' =>
+                array(
+                    0 => '001_install',
+                    1 => '002_migrate_0_1_1',
+                ),
+                'noviusos_page' =>
+                array(
+                    0 => '001_install',
+                    1 => '002_migrate_0_1_1',
+                    2 => '003_version_0_2',
+                ),
+                'noviusos_user' =>
+                array(
+                    0 => '001_install',
+                    1 => '002_migrate_0_1_1',
+                    2 => '003_version_0_2',
+                ),
+                'noviusos_templates_basic' =>
+                array(
+                    0 => '001_version_0_2',
+                ),
+            ));
+
         } else {
             parent::up();
         }
