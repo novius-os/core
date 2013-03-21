ebb26c38ae06535d026b33a0e366843d7bf6cc43
Sébastien Drouyer <drouyer@novius.com>
2013-03-21 15:32:19 +0100

    added dependencies configuration migration

---
+++ framework/migrations/003_new_dependencies_configuration.php
@@ -0,0 +1,27 @@
+<?php
+namespace Nos\Migrations;
+
+class New_Dependencies_Configuration extends \Nos\Migration
+{
+    public function up()
+    {
+        $config = \Nos\Config_Data::load('app_dependencies');
+        $has_changed = false;
+        foreach ($config as &$dependencies) {
+            $keys = array_keys($dependencies);
+            foreach ($keys as $key) {
+                if (!is_string($key)) {
+                    $has_changed = true;
+                    $extending_application = $dependencies[$key];
+                    unset($dependencies[$key]);
+                    $dependencies[$extending_application] = array(
+                        'extend_configuration' => true
+                    );
+                }
+            }
+        }
+        if ($has_changed) {
+            \Nos\Config_Data::save('app_dependencies', $config);
+        }
+    }
+}
