1aa470d7b3763f64fdb5d2dd67100577e4fe73e5
Julian Esperat <esperat@novius.fr>
2013-03-22 11:44:34 +0100

    Using a view for the publishable labels

--- framework/classes/orm/behaviour/publishable.php
+++ framework/classes/orm/behaviour/publishable.php
@@ -41,38 +41,9 @@ class Orm_Behaviour_Publishable extends Orm_Behaviour
 
     public function publication_status($item)
     {
-
-        $property = $this->_properties['publication_state_property'];
-        $status = $item->get($property);
-        if ($status == 0) {
-            return '<img class="publication_status" src="static/novius-os/admin/novius-os/img/icons/status-red.png"> '.__('Not published');
-        } else if ($status == 1) {
-            return '<img class="publication_status" src="static/novius-os/admin/novius-os/img/icons/status-green.png"> '.__('Published');
-        }
-
-        $start = $this->publication_start($item);
-        $end = $this->publication_end($item);
-        if (empty($start) && empty($end)) {
-            return '';
-        }
-
-        $now = strtotime('now');
-
-        if (!empty($start) && strtotime($start) > $now) {
-            return '<span class="publication_status ui-icon ui-icon-clock" /> '.strtr(__('Scheduled from {{date}}'), array(
-                '{{date}}' => \Date::forge(strtotime($start))->format(__('date_format.normal')),
-            ));
-        }
-        if (!empty($end) && strtotime($end) < $now) {
-            return '<img class="publication_status" src="static/novius-os/admin/novius-os/img/icons/status-red.png"> '.__('Not published');
-        }
-        if (!empty($end)) {
-            return '<img class="publication_status" src="static/novius-os/admin/novius-os/img/icons/status-green.png"> '.strtr(__('Published until {{date}}'), array(
-                '{{date}}' => \Date::forge(strtotime($end))->format(__('date_format.normal')),
-            ));
-        }
-
-        return '<img class="publication_status" src="static/novius-os/admin/novius-os/img/icons/status-green.png"> '.__('Published');
+        return (string) \View::forge('nos::admin/orm/publishable_label', array(
+            'item' => $item,
+        ), false);
     }
 
     public function planification_status($item)
