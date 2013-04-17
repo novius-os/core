fa5707aca5d14bfcec95ac1d9de5ca47922f00d5
Julian Esperat <esperat@novius.fr>
2013-04-17 10:20:59 +0200

    DATE_FORMAT_DEFAULT pattern

--- framework/lang/fr/common.lang.php
+++ framework/lang/fr/common.lang.php
@@ -6,6 +6,8 @@
 // 801 out of 801 words are translated (100%).
 
 return array(
+    'DATE_FORMAT_DEFAULT' => '%d %b %Y %H:%M',
+
     #: classes/controller/inspector/date.ctrl.php:147
     'Custom dates' => 'Choisir des dates',
 
