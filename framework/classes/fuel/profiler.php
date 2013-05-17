fa7e27cef011a544e0bb79744d0d65916fec354a
Gilles FELIX <felix@novius.com>
2013-05-17 11:50:37 +0200

    Update Profiler::start signature

--- framework/classes/fuel/profiler.php
+++ framework/classes/fuel/profiler.php
@@ -1,10 +1,10 @@
 <?php
 class Profiler extends \Fuel\Core\Profiler
 {
-    public static function start($dbname, $sql)
+    public static function start($dbname, $sql, $stacktrace = array())
     {
         \Event::trigger_function('profiler.sql', array(array('dbname' => $dbname, 'sql' => $sql)));
 
-        return parent::start($dbname, $sql);
+        return parent::start($dbname, $sql, $stacktrace);
     }
 }
