abebdac3d8600c9a140c7b407f06a2e9a607388e
Sébastien Drouyer <drouyer@novius.com>
2013-05-27 17:32:45 +0200

    Security class

--- framework/classes/fuel/security.php
+++ framework/classes/fuel/security.php
@@ -8,4 +8,13 @@ class Security extends \Fuel\Core\Security
 
         return html_entity_decode($string, $flags, $encoding);
     }
+
+    public static function htmlspecialchars($string, $flags = null, $encoding = null, $double_encode = null)
+    {
+        is_null($flags) and $flags = \Config::get('security.htmlentities_flags', ENT_QUOTES);
+        is_null($encoding) and $encoding = \Fuel::$encoding;
+        is_null($double_encode) and $double_encode = \Config::get('security.htmlentities_double_encode', false);
+
+        return htmlspecialchars($string, $flags, $encoding, $double_encode);
+    }
 }
