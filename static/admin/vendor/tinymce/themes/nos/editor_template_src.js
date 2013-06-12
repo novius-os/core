210233ce6c88c745b05e1d0eeaeecd2eace837f8
Gilles FELIX <felix@novius.com>
2013-06-12 15:24:12 +0200

    Bugfix : Wysiwyg, when open HTML editor, image lost her real URL

--- static/admin/vendor/tinymce/themes/nos/editor_template_src.js
+++ static/admin/vendor/tinymce/themes/nos/editor_template_src.js
@@ -301,11 +301,6 @@
 					var $img = $(this);
 					var media = $img.data('media');
                     var media_id = (media && media.id) ? media.id : $img.data('mediaId');
-					var src = 'nos://media/' + media_id;
-
-					if ($img.attr('width') && $img.attr('height')) {
-						src += '/' + $img.attr('width') + '/' + $img.attr('height');
-					}
 					return $('<img />').attr({
                         'data-media-id': media_id,
 						src:    $img.attr('src'),
@@ -327,26 +322,28 @@
 			ed.onPostProcess.add(function(ed, o) {
 				var content = $('<div></div>').html(o.content);
 
-				content.find('img').filter(function() {
-					return $(this).data('mediaId') || ($(this).data('media') || {}).id;// || $(this).attr('src').substr(0, 12) == 'nos://media/';
-				}).replaceWith(function() {
-					var $img = $(this);
-					var media = $img.data('media');
-                    var media_id = (media && media.id) ? media.id : $img.data('media-id');
-					var src = 'nos://media/' + media_id;
+                if (!o.source_view) {
+                    content.find('img').filter(function() {
+                        return $(this).data('mediaId') || ($(this).data('media') || {}).id;// || $(this).attr('src').substr(0, 12) == 'nos://media/';
+                    }).replaceWith(function() {
+                        var $img = $(this);
+                        var media = $img.data('media');
+                        var media_id = (media && media.id) ? media.id : $img.data('media-id');
+                        var src = 'nos://media/' + media_id;
 
-					if ($img.attr('width') && $img.attr('height')) {
-						src += '/' + $img.attr('width') + '/' + $img.attr('height');
-					}
-					var $new_img = $('<img />').attr({
-						src:    src,
-						title:  $img.attr('title'),
-						alt:    $img.attr('alt'),
-						style:  $img.attr('style')
-					});
-                    return $new_img;
-				});
-                o.content = content.html();
+                        if ($img.attr('width') && $img.attr('height')) {
+                            src += '/' + $img.attr('width') + '/' + $img.attr('height');
+                        }
+                        var $new_img = $('<img />').attr({
+                            src:    src,
+                            title:  $img.attr('title'),
+                            alt:    $img.attr('alt'),
+                            style:  $img.attr('style')
+                        });
+                        return $new_img;
+                    });
+                    o.content = content.html();
+                }
 			});
 
 			// Global onClick handlers to execute actions from the enhancers
