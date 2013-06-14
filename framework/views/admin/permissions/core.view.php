d0507592ffd899459f0169fe364e8e40625b3f0a
Julian Esperat <esperat@novius.fr>
2013-06-14 17:52:14 +0200

    Missing dictionnary

--- framework/views/admin/permissions/core.view.php
+++ framework/views/admin/permissions/core.view.php
@@ -3,7 +3,7 @@
 foreach ($list as $permission) {
 
     echo '<div class="permission '.$permission['permission_name'].'">';
-    echo '<h2>'.htmlspecialchars($permission['title']).'</h2>';
+    echo '<h2>'.$permission['title'].'</h2>';
     echo \View::forge($permission['view'], (!empty($permission['params']) ? $permission['params'] : array()) + array(
         'permission_name' => $permission['permission_name'],
         'checkbox_name' => 'perm['.$permission['permission_name'].'][]',
