27e6b5b5344162a108f8f6487603dfe2b215527b
Gilles FELIX <felix@novius.com>
2013-07-09 10:55:36 +0200

    Add a migration : renew cache for new images URL transformations

--- framework/migrations/005_images_url_transformations.php
+++ framework/migrations/005_images_url_transformations.php
@@ -1,6 +1,18 @@
 <?php
 namespace Nos\Migrations;
 
-class Wysiwyg_Twinnable extends \Nos\Migration
+class Images_Url_Transformations extends \Nos\Migration
 {
+    public function up()
+    {
+        try {
+            \File::delete_dir(\Config::get('cache_dir').'media', true, false);
+            \File::delete_dir(DOCROOT.'cache'.DS.'media', true, false);
+            \File::delete_dir(DOCROOT.'media', true, false);
+
+            \File::delete_dir(\Config::get('cache_dir').'pages', true, false);
+        } catch (\InvalidPathException $e) {
+            // Dir doesn't exists, no problem
+        }
+    }
 }
