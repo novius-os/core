5e2578bc918e755faf2d281b40e046a2ba6d4203
Gilles FELIX <felix@novius.com>
2013-07-19 11:32:09 +0200

    "call of Model_Media getImgTag and getImagTagResized change by htmlImg and htmlImgResized"

--- framework/views/admin/data_catcher/form.view.php
+++ framework/views/admin/data_catcher/form.view.php
@@ -126,7 +126,7 @@ echo \View::forge(
                         $media = \Nos\Media\Model_Media::find($media_id);
                         $field->set_options(
                             array(
-                                $media_id => $media->getImgTagResized(80, 80),
+                                $media_id => $media->htmlImgResized(80, 80),
                             )
                         );
                     }
