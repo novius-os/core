ea91ac87388f54e0222e19771135f3c9e63d543a
Gilles FELIX <felix@novius.com>
2013-10-01 17:18:08 +0200

    Bugfix : patch for profiler display EXPLAIN query containing accent

--- framework/classes/fuel/profiler.php
+++ framework/classes/fuel/profiler.php
@@ -5,6 +5,13 @@ class Profiler extends \Fuel\Core\Profiler
     {
         \Event::trigger_function('profiler.sql', array(array('dbname' => $dbname, 'sql' => $sql)));
 
+        // Patch, phpquickprofiler attemptToExplainQuery() function use html_entity_decode without encoding parameter
+        // UTF-8 is the default encoding only since PHP 5.4
+        // Without the Profiler generate warning when display EXPLAIN request containing accent
+        if (\Fuel::$encoding === 'UTF-8' && version_compare(PHP_VERSION, '5.4.0') < 0) {
+            $sql = utf8_encode($sql);
+        }
+
         return parent::start($dbname, $sql, $stacktrace);
     }
 }
