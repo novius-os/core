7099bdba3e848730477e57ae9f92094f5a776c7d
Gilles FELIX <felix@novius.com>
2013-11-13 17:30:52 +0100

    Refactoring tinymce end

--- framework/config/wysiwyg.config.php
+++ framework/config/wysiwyg.config.php
@@ -14,7 +14,7 @@ return array(
         'doctype' => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
         'theme' => 'nos',
         'skin' => 'bootstrap',
-        'plugins' => 'spellchecker,xhtmlxtras,style,table,advlist,inlinepopups,media,searchreplace,nospaste,noneditable,nonbreaking,nosenhancer,noslink,nosalign,nosbrclearall,nostoolbartoggle,nosvisualhtml,nosimage,nosstyleselect',
+        'plugins' => 'spellchecker,xhtmlxtras,style,table,advlist,inlinepopups,media,searchreplace,noneditable,nonbreaking',
         'paste_text_use_dialog' => true,
     ),
 
@@ -27,7 +27,7 @@ return array(
             'theme_advanced_buttons1' => "tablecontrols",
             'theme_advanced_buttons2' => "underline,strikethrough,sub,sup,|,forecolor,backcolor,|,outdent,indent,blockquote,|,anchor,charmap,hr,nonbreaking,nosbrclearall,|,styleprops,removeformat",
             'theme_advanced_buttons3' => "search,replace,|,spellchecker,|,newdocument,nosvisualhtml,code",
-            'theme_advanced_buttons4' => "nosimage,nosmedia,noslink,nosenhancer",
+            'theme_advanced_buttons4' => "nosimage,mediawithlabel,noslink,nosenhancer",
             'theme_advanced_buttons5' => "nosstyleselect,bold,italic,nosalign,bullist,numlist,|,cut,copy,nospaste,undo,redo,|,nostoolbartoggle",
         ),
     ),
