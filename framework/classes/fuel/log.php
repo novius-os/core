06f358666505e6b9ffa1bcce3b91b02378d8d8a5
Gilles FELIX <felix@novius.com>
2013-11-28 17:03:18 +0100

    Coding style

--- framework/classes/fuel/log.php
+++ framework/classes/fuel/log.php
@@ -35,7 +35,8 @@ class Log extends Fuel\Core\Log
         logger(\Fuel::L_WARNING, $log);
     }
 
-    public static function exception($e, $prefix = '') {
+    public static function exception($e, $prefix = '')
+    {
         \Log::error($prefix.$e->getCode().' - '.$e->getMessage().' in '.$e->getFile().' on line '.$e->getLine());
     }
 }
