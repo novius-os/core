c2898e97444b6e3b0dbf05f73ba3d0f27203a719
Gilles FELIX <felix@novius.com>
2013-12-02 12:06:24 +0100

    Fix regression due to temporary removal of sensibal config items in profiler output

--- framework/classes/fuel/profiler.php
+++ framework/classes/fuel/profiler.php
@@ -21,21 +21,35 @@ class Profiler extends \Fuel\Core\Profiler
         if (static::$profiler) {
             $items = \Config::$items;
 
-            \Arr::delete(\Config::$items, 'db');
-            \Arr::delete(\Config::$items, 'crypt');
-            \Arr::delete(\Config::$items, 'email');
-            \Arr::delete(\Config::$items, 'ftp');
-
             array_walk_recursive(\Config::$items, function (&$item, $key) {
-                if (stripos($key, 'password') !== false || stripos($key, 'pwd') !== false) {
-                    $item = '(hidden value)';
-                }
                 if (is_string($item)) {
                     $item = Fuel::clean_path($item);
                 }
             });
 
             $output = static::$profiler->display(static::$profiler);
+
+            $replace = function ($key, $value) use (&$output) {
+                if (!is_bool($value) && !is_null($value) && !is_array($value) && !is_object($value)) {
+                    $output = preg_replace(
+                        '`<b>'.\Security::htmlentities($value).'</b>((&rsaquo;&nbsp;)*)'.$key.'`',
+                        '<b><i><s>(hidden value)</s></i></b>$1'.$key,
+                        $output
+                    );
+                }
+            };
+
+            array_walk_recursive(\Config::$items, function ($item, $key) use ($replace) {
+                if (stripos($key, 'password') !== false || stripos($key, 'pwd') !== false ||
+                    stripos($key, 'host') !== false || stripos($key, 'username') !== false ||
+                    stripos($key, 'database') !== false) {
+                    $replace($key, $item);
+                }
+            });
+
+            foreach (\Arr::get($items, 'crypt', array()) as $key => $value) {
+                $replace($key, $value);
+            }
 
             \Config::$items = $items;
         }
