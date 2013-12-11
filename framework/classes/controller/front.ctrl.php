novius-os/core

commit be9211f001b5713bb6512bfab6b05ab242103dc6

factorized cache file path

framework/classes/controller/front.ctrl.php
@@ -100,9 +100,9 @@ class Controller_Front extends Controller
         \Event::trigger('front.start');
         \Event::trigger_function('front.start', array(array('url' => &$url, 'cache_path' => &$cache_path)));
 
-        $cache_path = str_replace(array('http://', 'https://', '/'), array('', '', '_'), rtrim($this->_base_href, '/')).DS.rtrim($cache_path, '/');
+        $cache_path = \Nos\FrontCache::getPathFromUrl($this->_base_href, $this->_url);
 
-        $this->_cache = FrontCache::forge('pages'.DS.$cache_path);
+        $this->_cache = FrontCache::forge($cache_path);
 
         try {
             if (!$this->_use_cache) {

framework/applications/noviusos_page/classes/model/page.model.php
@@ -333,17 +333,7 @@ class Model_Page extends \Nos\Orm\Model
         $url = $this->page_entrance ? '' : ltrim($this->virtual_path(), '/');
         $contexts = \Nos\Tools_Context::contexts();
         foreach ($contexts[$this->page_context] as $context_url) {
-            $host = parse_url($context_url, PHP_URL_HOST);
-            $path = ltrim(parse_url($context_url, PHP_URL_PATH), '/');
-
-            $url = $path.$url;
-            $cache_path = (empty($url) ? 'index/' : $url);
-            // This event mostly redirects, don't trigger it
-            //\Event::trigger_function('front.start', array(array('url' => &$url, 'cache_path' => &$cache_path)));
-            $cache_path = $host.DS.rtrim($cache_path, '/');
-
-            // Delete file
-            $cache = \Nos\FrontCache::forge('pages'.DS.$cache_path);
+            $cache = \Nos\FrontCache::forge(\Nos\FrontCache::getPathFromUrl($context_url, $url));
             $cache->delete();
         }
 

framework/classes/frontcache.php
@@ -376,4 +376,9 @@ class FrontCache
     {
         return $this->_path;
     }
+
+    public static function getPathFromUrl($base, $url) {
+        $url = (empty($url) ? 'index/' : $url);
+        return 'pages'.DS.str_replace(array('http://', 'https://', '/'), array('', '', '_'), rtrim($base, '/')).DS.rtrim($url, '/');
+    }
 }

commit c79007725e2b983de07858059ca054782a65784a

Bugfix: take modifications of cache_path in event front.start into account
Fix a regression introduce in be9211f001b5713bb6512bfab6b05ab242103dc6

The modification of $cache_path in the event front.start was no longer used.
It was working in chiba.1, and no longer working in chiba.2.

framework/classes/controller/front.ctrl.php
@@ -102,7 +102,7 @@ class Controller_Front extends Controller
 
         \Event::trigger_function('front.start', array(array('url' => &$url, 'cache_path' => &$cache_path)));
 
-        $cache_path = \Nos\FrontCache::getPathFromUrl($this->_base_href, $this->_url);
+        $cache_path = \Nos\FrontCache::getPathFromUrl($this->_base_href, $cache_path);
 
         $this->_cache = FrontCache::forge($cache_path);
 