novius-os/core

commit 4b0dd69793775b74dcd248e2c6f19df3e1cd80d1

Considers cache dirty if cache duration changes

framework/classes/frontcache.php
@@ -22,6 +22,8 @@ class FrontCache
 {
     protected static $_php_begin = null;
 
+    public static $cache_duration = 60;
+
     /**
      * Loads any default caching settings when available
      */
@@ -215,9 +217,12 @@ class FrontCache
         $this->_level = ob_get_level();
     }
 
-    public static function checkExpires($expires)
+    public static function checkExpires($expires, $initial_cache_duration)
     {
         if ($expires > 0 && $expires <= time()) {
+            throw new CacheExpiredException();
+        }
+        if ($initial_cache_duration > static::$cache_duration) {
             throw new CacheExpiredException();
         }
     }
@@ -234,7 +239,7 @@ class FrontCache
             $expires = time() + $duration;
             $prepend .= '<?php
 
-            '.__CLASS__.'::checkExpires('.$expires.');'."\n";
+            '.__CLASS__.'::checkExpires('.$expires.', '.$duration.');'."\n";
 
             if (!empty($controller)) {
                 if ($this->_path === $this->_init_path && !empty($this->_suffix_handlers)) {

framework/classes/controller/front.ctrl.php
@@ -105,6 +105,7 @@ class Controller_Front extends Controller
         $cache_path = \Nos\FrontCache::getPathFromUrl($this->_base_href, $cache_path);
 
         $this->_cache = FrontCache::forge($cache_path);
+        \Nos\FrontCache::$cache_duration = $this->_cache_duration;
 
         try {
             if (!$this->_use_cache) {

commit 22988b67bda531858dedc49abe5afba73cf04827

Bugfix: \Nos\FrontCache::get() doesn't work if duration>60
Bug introduced in 4b0dd69793775b74dcd248e2c6f19df3e1cd80d1

framework/classes/frontcache.php
@@ -96,12 +96,20 @@ class FrontCache
         );
 
         $cache = new static($path);
+
+        $old_cache_duration = static::$cache_duration;
+        static::$cache_duration = $params['duration'];
+
         try {
-            return $cache->executeOrStart($params['controller']);
+            $result = $cache->executeOrStart($params['controller']);
+            static::$cache_duration = $old_cache_duration;
+            return $result;
         } catch (CacheNotFoundException $e) {
             call_user_func_array($params['callback_func'], $params['callback_args']);
 
-            return $cache->saveAndExecute($params['duration'], $params['controller']);
+            $result = $cache->saveAndExecute($params['duration'], $params['controller']);
+            static::$cache_duration = $old_cache_duration;
+            return $result;
         }
     }
 