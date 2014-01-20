be7e6c55da4a574e97ec0058a86ff6e3d24fbe54
Gilles FELIX <felix@novius.com>
2014-01-20 11:06:20 +0100

    "Version Chiba 2.4"

--- framework/config/version.config.php
+++ framework/config/version.config.php
@@ -9,6 +9,6 @@
  */
 
 return array(
-    'fullname' => 'Chiba 2.3.3',
-    'date'     => \Date::create_from_string('2013-12-20', 'mysql_date'),
+    'fullname' => 'Chiba 2.4',
+    'date'     => \Date::create_from_string('2014-01-20', 'mysql_date'),
 );
