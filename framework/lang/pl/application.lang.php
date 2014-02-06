da92700242d8aca264a9cc494e34c5517c46a999
Jiboule <cadot@novius.fr>
2014-02-06 12:20:18 +0100

    Add the polish language

---
+++ framework/lang/pl/application.lang.php
@@ -0,0 +1,62 @@
+<?php
+
+// Generated on 19/08/2013 18:12:14
+
+// 17 out of 17 messages are translated (100%).
+// 98 out of 98 words are translated (100%).
+
+return array(
+    #. Note to translator: This is a submit button
+    #: classes/controller/admin/crud.ctrl.php:280
+    'Add' => 'Dodaj',
+
+    #: classes/controller/admin/crud.ctrl.php:369
+    'This item already exists in {{context}}. Therefore your item cannot be added.' => 'Ten element już istnieje w {{context}}. Dlatego twój element nie może być dodany.',
+
+    #: classes/controller/admin/appdesk.ctrl.php:165
+    'Default view' => 'Widok domyślny',
+
+    #: views/crud/context_common_fields.view.php:17
+    'This field is common to all languages' => 'To pole jest wspólne dla wszystkich języków',
+
+    #: views/crud/context_common_fields.view.php:17
+    'This field is common to all contexts' => 'To pole jest wspólne dla wszystkich sformułowań',
+
+    #: views/crud/context_common_fields.view.php:19
+    'When you modify the value of this field, the change applies to the following languages:' => 'Jeśli zmieniasz wartość tego pola, to zmiana dotyczy również następujących języków :',
+
+    #: views/crud/context_common_fields.view.php:20
+    'When you modify the value of this field, the change applies to the following contexts:' => 'Jeśli zmieniasz wartość tego pola, to zmiana dotyczy również następujących sformułowań :',
+
+    #: views/crud/context_common_fields.view.php:21
+    'Go ahead, I understand' => 'kontynuuj, rozumiem',
+
+    #: views/crud/context_common_fields.view.php:22
+    'Cancel, I won’t modify it' => 'Anuluj, nie chcę tego zmieniać',
+
+    #. Note to translator: this is an action (button)
+    #: views/crud/blank_slate.view.php:61
+    'Translate from {{context}}' => 'Przetłumaczyć z {{context}}',
+
+    #: views/crud/blank_slate.view.php:70
+    'Add ‘{{item}}’ to {{context}}' => 'Dodaj {{item}} do {{context}}',
+
+    #: views/crud/blank_slate.view.php:72
+    'Translate ‘{{item}}’ into {{context}}' => 'Przetłumacz {{item}} na {{context}}',
+
+    #: views/crud/blank_slate.view.php:78
+    'You have two options:' => 'Masz dwie możliwości :',
+
+    #: views/crud/blank_slate.view.php:88
+    'Start from scratch' => 'Zacznij od początku',
+
+    #: views/crud/blank_slate.view.php:90
+    '(Blank form)' => '(Pusty formularz)',
+
+    #: views/crud/blank_slate.view.php:93
+    'OR' => 'ALBO',
+
+    #: views/crud/blank_slate.view.php:118
+    '(Form filled with the contents from the original version)' => '(Formularz wypełniony treścią w wersji oryginalnej)',
+
+);
