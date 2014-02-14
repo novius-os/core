263f9e3846ad7d6d2fa4a020b3a9e3536d3d193d
Gilles FELIX <felix@novius.com>
2014-02-14 14:24:20 +0100

    TinyMCE : move media treatments from enhancer plugin to media plugin Bugfix: in page, when switch template, images in wysiwyg are broken

--- static/admin/vendor/tinymce/plugins/nosimage/editor_plugin_src.js
+++ static/admin/vendor/tinymce/plugins/nosimage/editor_plugin_src.js
@@ -13,7 +13,12 @@
 
     tinymce.create('tinymce.plugins.NosImagePlugin', {
         init : function(ed, url) {
-            var self = this, mediaCached = {};
+            var self = this, mediaCached;
+
+            if (!tinymce.nosMediaCached) {
+                tinymce.nosMediaCached = {};
+            }
+            mediaCached = tinymce.nosMediaCached;
 
             self.editor = ed;
 
@@ -51,6 +56,75 @@
                     var temp =!co && !!p && n.className.indexOf('mceItem') == -1;
                     c.setActive(!co && !!p && n.className.indexOf('mceItem') == -1);
                 }
+            });
+
+            function mediaTransformSrcIntoNos(content) {
+                content.find('img').each(function() {
+                    var $img = $(this);
+                    var media_id = $img.attr('data-media-id');
+                    if (!media_id) {
+                        return;
+                    }
+                    var origSrc = $img.attr('src');
+                    if (origSrc.substr(0, 6) !== 'nos://') {
+                        var src = 'nos://media/' + media_id;
+                        var width = $img.attr('width');
+                        var height = $img.attr('height');
+
+                        if (width && height) {
+                            src += '/' + width + '/' + height;
+                            var resolution = width * height;
+                        } else {
+                            resolution = 0;
+                        }
+
+                        // Store src in the cache when: 1. it doesn't exists yet or 2. the resolution is higher than the stored one
+                        // resolution == 0 means it's the larger possible size
+                        if (resolution == 0 || !mediaCached[media_id] || resolution > mediaCached[media_id].resolution) {
+                            mediaCached[media_id] = {
+                                attrs : {
+                                    src: $img.attr('src'),
+                                    'data-media-id' : media_id
+                                },
+                                resolution: resolution
+                            };
+                        }
+
+                        $img.attr({
+                            src: src
+                        }).removeClass('nosMedia').removeAttr('data-media').removeAttr('data-media-id');
+                    }
+                    $img.removeData().attr('data-mce-src', origSrc).removeAttr('data-mce-src');
+                });
+            }
+
+            function mediaRestoreSrc(content) {
+                content.find('img').each(function() {
+                    var $img = $(this);
+                    $img.removeData().removeAttr('data-mce-src');
+                    var origSrc = $img.attr('src');
+                    if (origSrc.substr(0, 6) == 'nos://') {
+                        // remove 'nos://media/'
+                        var media_id = origSrc.substr(12).split('/')[0];
+                        if (media_id && mediaCached[media_id]) {
+                            $img.attr(mediaCached[media_id]['attrs']).addClass('nosMedia');
+                        }
+                    }
+                });
+            }
+
+            // When editing HTML content, we clean up enhancer preview, we'll make them nice again after
+            ed.onGetContent.add(function(ed, o) {
+                var content = $(o.content);
+                mediaTransformSrcIntoNos(content);
+                o.content = $('<div></div>').append(content).html();
+            });
+
+            ed.onSetContent.add(function(ed, o) {
+                setTimeout(function() {
+                    var content = $(ed.getBody());
+                    mediaRestoreSrc(content);
+                }, 1);
             });
         },
 
