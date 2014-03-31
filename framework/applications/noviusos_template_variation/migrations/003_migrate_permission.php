6a472867a3946fa0d8f45bcdd7892b67ffc4624b
Gilles FELIX <felix@novius.com>
2014-03-31 11:15:51 +0200

    Bugfix: on fresh install for template variation

--- framework/applications/noviusos_template_variation/migrations/003_migrate_permission.php
+++ framework/applications/noviusos_template_variation/migrations/003_migrate_permission.php
@@ -10,26 +10,12 @@
 
 namespace Nos\Template\Variation\Migrations;
 
-use Nos\Template\Variation\Model_Template_Variation;
+use Nos\Migration;
 
-class Migrate_Template extends \Nos\Migration
+class Migrate_Permission extends Migration
 {
     public function up()
     {
-        parent::up();
-
-        $templates = \Nos\Config_Data::get('templates', array());
-
-        $templates_context = \DB::select('page_template', 'page_context')->from('nos_page')->distinct()->execute();
-        foreach ($templates_context->as_array() as $template_context) {
-            $template = \Arr::get($templates, $template_context['page_template'], array());
-            $template_variation = Model_Template_Variation::forge();
-            $template_variation->tpvar_template = $template_context['page_template'];
-            $template_variation->tpvar_title = \Arr::get($template, 'title', $template_context['page_template']);
-            $template_variation->tpvar_context = $template_context['page_context'];
-            $template_variation->save();
-        }
-
         $permission = \Db::list_tables('nos_role_permission');
         if (!empty($permission)) {
             \DB::delete('nos_role_permission')
