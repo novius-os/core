a5de33f121df90dfe95e22caafb6ffa66bb3a1c3
Gilles FELIX <felix@novius.com>
2014-05-22 16:04:23 +0200

    Update jQuery datetimepicker to 1.4.4

--- static/admin/vendor/jquery/ui-datetimepicker/i18n/jquery-ui-i18n.js
+++ static/admin/vendor/jquery/ui-datetimepicker/i18n/jquery-ui-i18n.js
@@ -1,3 +1,24 @@
+/* Spanish translation for the jQuery Timepicker Addon */
+/* Written by Ianaré Sévi */
+(function($) {
+    $.timepicker.regional['es'] = {
+        timeOnlyTitle: 'Elegir una hora',
+        timeText: 'Hora',
+        hourText: 'Horas',
+        minuteText: 'Minutos',
+        secondText: 'Segundos',
+        millisecText: 'Milisegundos',
+        microsecText: 'Microsegundos',
+        timezoneText: 'Huso horario',
+        currentText: 'Ahora',
+        closeText: 'Cerrar',
+        timeFormat: 'HH:mm',
+        amNames: ['a.m.', 'AM', 'A'],
+        pmNames: ['p.m.', 'PM', 'P'],
+        isRTL: false
+    };
+})(jQuery);
+
 /* French translation for the jQuery Timepicker Addon */
 /* Written by Thomas Lété */
 (function($) {
@@ -8,6 +29,7 @@
         minuteText: 'Minutes',
         secondText: 'Secondes',
         millisecText: 'Millisecondes',
+        microsecText: 'Microsecondes',
         timezoneText: 'Fuseau horaire',
         currentText: 'Maintenant',
         closeText: 'Terminé',
@@ -28,12 +50,34 @@
         minuteText: '分',
         secondText: '秒',
         millisecText: 'ミリ秒',
+        microsecText: 'マイクロ秒',
         timezoneText: 'タイムゾーン',
         currentText: '現時刻',
         closeText: '閉じる',
         timeFormat: 'HH:mm',
         amNames: ['午前', 'AM', 'A'],
         pmNames: ['午後', 'PM', 'P'],
+        isRTL: false
+    };
+})(jQuery);
+
+/* Russian translation for the jQuery Timepicker Addon */
+/* Written by Trent Richardson */
+(function($) {
+    $.timepicker.regional['ru'] = {
+        timeOnlyTitle: 'Выберите время',
+        timeText: 'Время',
+        hourText: 'Часы',
+        minuteText: 'Минуты',
+        secondText: 'Секунды',
+        millisecText: 'Миллисекунды',
+        microsecText: 'Микросекунды',
+        timezoneText: 'Часовой пояс',
+        currentText: 'Сейчас',
+        closeText: 'Закрыть',
+        timeFormat: 'HH:mm',
+        amNames: ['AM', 'A'],
+        pmNames: ['PM', 'P'],
         isRTL: false
     };
 })(jQuery);
