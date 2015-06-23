aa46db6fb2c85bc25720089222d5d44d8e9a2870
Pascal VINEY <shaoshiva@gmail.com>
2015-06-24 00:14:17 +0200

    Getters for meta title/description/keywords

--- framework/applications/noviusos_page/classes/model/page.model.php
+++ framework/applications/noviusos_page/classes/model/page.model.php
@@ -366,6 +366,32 @@ class Model_Page extends \Nos\Orm\Model
         return $url;
     }
 
+    /**
+     * Gets the meta title
+     * 
+     * @return bool|string
+     */
+    public function getMetaTitle() {
+        return !$page->page_meta_noindex ? $page->page_meta_title : null;
+    }
+
+    /**
+     * Gets the meta description
+     * 
+     * @return bool|string
+     */
+    public function getMetaDescription() {
+        return !$page->page_meta_noindex ? $page->page_meta_description : null;
+    }
+
+    /**
+     * Gets the meta keywords
+     * 
+     * @return bool|string
+     */
+    public function getMetaKeywords() {
+        return !$page->page_meta_noindex ? $page->page_meta_keywords : null;
+    }
 
     /**
      *  Delete the cache for this page
