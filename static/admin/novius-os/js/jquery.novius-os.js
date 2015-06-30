6b8f96aeab6f90c531204cc2d5c9f30687771317
Julien Guyomard <guyomard@novius.fr>
2015-06-30 11:14:18 +0200

    Escape media_title

--- static/admin/novius-os/js/jquery.novius-os.js
+++ static/admin/novius-os/js/jquery.novius-os.js
@@ -111,6 +111,15 @@ define('jquery-nos',
                 errorImageNotfind : 'We’re afraid we cannot find this image.'
             },
 
+            nosEscapeHtml: function(str) {
+                return str
+                    .replace(/&/g, "&amp;")
+                    .replace(/</g, "&lt;")
+                    .replace(/>/g, "&gt;")
+                    .replace(/"/g, "&quot;")
+                    .replace(/'/g, "&#039;");
+            },
+
             nosI18nPlural: function(msgs, $n) {
                 var plural_indice;
                 if (!$.isArray(msgs)) {
@@ -339,7 +348,7 @@ define('jquery-nos',
                             .find('img')
                             .attr({
                                 src : media.path,
-                                title : media.title
+                                title : $.nosEscapeHtml(media.title)
                             })
                             .css({
                                 width : 0,
