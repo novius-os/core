8e0582329ab8654bb40f1e4ccad7ba22de805354
Pascal VINEY <shaoshiva@gmail.com>
2015-07-02 13:37:06 +0200

    Fix code indentation

--- framework/classes/orm/twinnable/manymany.php
+++ framework/classes/orm/twinnable/manymany.php
@@ -163,15 +163,15 @@ class Orm_Twinnable_ManyMany extends \Orm\ManyMany
      */
     public function join($alias_from, $rel_name, $alias_to_nr, $conditions = array())
     {
-		// Consider as a classical many_many relation if no column context defined
+	// Consider as a classical many_many relation if no column context defined
         if (!$this->column_context_to) {
-			// @todo throw an exception in a future major release ?
+            // @todo throw an exception in a future major release ?
             return parent::join($alias_from, $rel_name, $alias_to_nr, $conditions);
         }
 
-		$this->alias_to = 't'.$alias_to_nr;
-		$this->alias_from = $alias_from;
-		$this->alias_through = $this->alias_to.'_through';
+        $this->alias_to = 't'.$alias_to_nr;
+        $this->alias_from = $alias_from;
+        $this->alias_through = $this->alias_to.'_through';
 
         // Merges the conditions of the relation with the specific conditions
         $conditions = \Arr::merge($this->conditions, $conditions);
