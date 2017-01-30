13a37265640a1b7d974d1f93551bd6904b3730f2
mesa <mesa@novius.fr>
2017-01-30 10:46:28 +0100

    encode paths without modifying url slashes

--- framework/applications/noviusos_media/classes/model/media.model.php
+++ framework/applications/noviusos_media/classes/model/media.model.php
@@ -374,7 +374,7 @@ class Model_Media extends \Nos\Orm\Model
      */
     public function url($absolute = true)
     {
-        return ($absolute ? \Uri::base(false) : '').static::$public_path.urlencode($this->_getVirtualPath());
+        return \Nos\Tools_Url::encodePath(($absolute ? \Uri::base(false) : '').static::$public_path.$this->_getVirtualPath(), $absolute);
     }
 
     /**
